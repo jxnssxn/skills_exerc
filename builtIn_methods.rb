580caf94519e43d750f46bdbd2576a182fcdb73e
jimmy <jonssonjimmy8@gmail.com>
2020-10-29 18:49:23 -0400

    upcase method practice

---
+++ builtIn_methods.rb
@@ -0,0 +1,5 @@
+# 1. Write a program that asks the user to enter a word, then prints that word with all capital letters.
+
+puts "what is the longest word you know?"
+input = gets.chomp
+p input.upcase
