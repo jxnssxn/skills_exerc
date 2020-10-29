cc67cbe98c8b8e65096dd13fee79ec9ec2105e60
jimmy <jonssonjimmy8@gmail.com>
2020-10-29 18:08:54 -0400

    string exercises 6 through 9

--- string_practice.rb
+++ string_practice.rb
@@ -23,6 +23,8 @@ input = gets.chomp
 
 if input == "marco"
   puts "polo"
+else
+  puts "Try again."
 end
 
 # 4. Write a program that uses variables to store three different colors, then prints out a sentence using the colors with string concatenation (the + operator).
@@ -43,3 +45,43 @@ three = "forest green"
 my_fav_colors = "My favorite color palette is #{three}, #{two} and #{one}."
 
 p my_fav_colors
+
+# 6. Write a program that asks the user to enter a name. If the name is not "Santa", print "You're not Santa."
+puts "Enter a name: "
+input = gets.chomp
+
+if input != "Santa"
+  p "You're not Santa."
+else
+  p "I want all brand new home tech!"
+end
+
+# 7. Write a program that uses variables to store a book's title and author, then prints out a sentence using that information with string concatenation (the + operator).
+
+book_title = "Alkemisten"
+book_author = "Paulo Coelho"
+
+fav_book = "My favorite book is titled " + book_title + "and it was written by " + book_author + "."
+
+p fav_book
+
+# 8. Write a program that uses variables to store a book's title and author, then prints out a sentence using that information with string interpolation (the #{} operator).
+
+book_title = "The Devil and Miss Prym"
+book_author = "Paulo Coelho"
+
+good_book = "Another intriguing book by #{book_author} is #{book_title}."
+
+p good_book
+
+# 9. Write a program that asks the user to enter a password. If the password is "Joshua", the program responds "Shall we play a game?". For any other password, the program responds "Access denied"
+
+puts "Enter a password: "
+
+password = gets.chomp
+
+if password == "Joshua"
+  puts "Shall we play a game?"
+else
+  puts "Access denied"
+end
