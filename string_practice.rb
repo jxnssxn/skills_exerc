221042e0d31ca4793b2eeae88369bd5f11e5b415
jimmy <jonssonjimmy8@gmail.com>
2020-10-29 17:25:27 -0400

    2nd string practice exercise

--- string_practice.rb
+++ string_practice.rb
@@ -5,3 +5,12 @@ last_name = "Anderson"
 full_name = first_name + " " + last_name
 
 p full_name
+
+# 2. Write a program that uses variables to store a first and last name, then prints the full name in one line using string interpolation (the #{} operator).
+
+first_nombre = "Karl"
+last_nombre = "Hernandez"
+
+full_nombre = "My friend's name is #{first_nombre} #{last_nombre}."
+
+p full_nombre
