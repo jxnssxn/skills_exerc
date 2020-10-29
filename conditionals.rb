7be9b7dc61a1cf61a0cc9fdcd279b7c031c4413e
jimmy <jonssonjimmy8@gmail.com>
2020-10-29 14:28:10 -0400

    first conditional exercise from skills sheet

---
+++ conditionals.rb
@@ -0,0 +1,9 @@
+#create if statements from scratch
+# 1. Use a variable to store a number, then write a condition that prints 0 if the number is equal to 10, and prints -1 otherwise.
+
+x = 5
+if x == 10
+  p 0
+elsif
+  p -1
+end
