bb9c29d46e12a9762cd7816205be000ec44ec2a8
jimmy <jonssonjimmy8@gmail.com>
2020-10-29 15:12:37 -0400

    4thd conditional exercise

--- conditionals.rb
+++ conditionals.rb
@@ -40,17 +40,3 @@ if over9 > 9000
 elsif
   p -1
 end
-
-# 5. Use a variable to store a number, then write a condition that prints 9 if the number is less than 10, prints 19 if the number is less than 20, prints 29 if the number is less than 30, and prints -1 otherwise (only one print statement should occur).
-
-num = 22
-
-if num < 10
-  p 9
-elsif num < 20
-  p 19
-elsif num < 30
-  p 29
-else
-  p -1
-end
