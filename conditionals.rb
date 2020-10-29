ac4e19d3470187bb860bf9cc793c52914d0e9f15
jimmy <jonssonjimmy8@gmail.com>
2020-10-29 14:50:54 -0400

    2nd conditional exercise

--- conditionals.rb
+++ conditionals.rb
@@ -7,3 +7,15 @@ if x == 10
 elsif
   p -1
 end
+
+# 2. Use a variable to store a number, then write a condition that prints -1 if the number is less than 10, prints 1 if the number is greater than 10, and prints 0 if the number is equal to 10.
+
+z = 5
+
+if z < 10
+  p -1
+elsif z > 10
+  p 1
+else 
+  p 0
+end
