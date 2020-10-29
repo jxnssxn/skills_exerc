d1f6448dfb4cbaa512e45d00b8a77491074f7404
jimmy <jonssonjimmy8@gmail.com>
2020-10-29 17:21:37 -0400

    1st string practice exercise

---
+++ string_practice.rb
@@ -0,0 +1,7 @@
+# 1. Write a program that uses variables to store a first and last name, then prints the full name in one line using string concatenation (the + operator).
+first_name = "Mario"
+last_name = "Anderson"
+
+full_name = first_name + " " + last_name
+
+p full_name
