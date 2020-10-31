3f6486f89a871244a5bf6a71937bed5e47472540
jimmy <jonssonjimmy8@gmail.com>
2020-10-31 12:02:34 -0400

    method exercise 33

--- builtIn_methods.rb
+++ builtIn_methods.rb
@@ -1,18 +1,24 @@
 # 1. Write a program that asks the user to enter a word, then prints that word with all capital letters.
 
-puts "what is the longest word you know?"
-input = gets.chomp
-p input.upcase
+# puts "what is the longest word you know?"
+# input = gets.chomp
+# p input.upcase
 
-# 2. Write a program that asks the user to enter a number, then prints "That's a big number" if the number is greater than 100.
+# # 2. Write a program that asks the user to enter a number, then prints "That's a big number" if the number is greater than 100.
 
-puts "Enter a number a number: "
+# puts "Enter a number a number: "
 
-number = gets.chomp
+# number = gets.chomp
 
-if input.to_i < 100
-  p "That's a big number"
-end
+# if input.to_i < 100
+#   p "That's a big number"
+# end
 
 # 3. Write a program that asks the user to enter two numbers, then prints the numbers added together.
 
+
+puts "Enter a number: "
+number = gets.chomp
+puts "Enter a 2nd number: "
+number2 = gets.chomp
+p number.to_i + number2.to_i
