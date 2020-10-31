f71936795af5f41be7e7f0f060a2046c95b4e187
jimmy <jonssonjimmy8@gmail.com>
2020-10-31 14:11:52 -0400

    methods exercise 4 thru 9

--- builtIn_methods.rb
+++ builtIn_methods.rb
@@ -17,8 +17,52 @@
 # 3. Write a program that asks the user to enter two numbers, then prints the numbers added together.
 
 
-puts "Enter a number: "
-number = gets.chomp
-puts "Enter a 2nd number: "
-number2 = gets.chomp
-p number.to_i + number2.to_i
+# puts "Enter a number: "
+# number = gets.chomp
+# puts "Enter a 2nd number: "
+# number2 = gets.chomp
+# p number.to_i + number2.to_i
+
+# 4. Write a program that asks the user to enter a word, then prints that word in reverse order.
+
+# puts "Enter your name: "
+
+# name = gets.chomp
+# p name.reverse
+
+
+# 5. Write a program that asks the user to enter a number, then prints the number times 10.
+
+# puts "whats your favorite number?"
+# number = gets.chomp
+# p number.to_i * 10
+
+# 6. Write a program that asks the user to enter two words, then prints both words on the same line in all capital letters.
+
+# puts "what are the two words you say the most?"
+# words_used = gets.chomp
+# p words_used.upcase
+
+# 7. Write a program that asks the user to enter a word, then prints the number of letters in the word.
+
+# puts "Type a word that has more than 5 letters:"
+# wordy = gets.chomp
+# p wordy.size
+
+# 8. Write a program that asks the user to enter a number, then prints "That's a negative number" if the number is less than 0.
+
+# puts "What's the smallest number you can think of?"
+
+# small_num = gets.chomp
+# if small_num.to_i < 0
+#   p "That's a negative number"
+# end
+
+# 9. Write a program that asks the user to enter two numbers, then prints the two numbers multiplied together.
+
+puts "Provide two numbers: "
+one_num = gets.chomp
+two_num = gets.chomp
+p two_num.to_i * two_num.to_i
+
+# 10. Write a program that asks the user to enter a word, then prints "That's a long word" if the word has more than 5 letters.
