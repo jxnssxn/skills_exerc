90f35e57c786e5d8de04b05f579fbfc24ed7f8c7
jimmy <jonssonjimmy8@gmail.com>
2020-10-31 14:26:14 -0400

    methods exercise 10

--- builtIn_methods.rb
+++ builtIn_methods.rb
@@ -1,62 +1,61 @@
 # 1. Write a program that asks the user to enter a word, then prints that word with all capital letters.
 
-# puts "what is the longest word you know?"
-# input = gets.chomp
-# p input.upcase
+puts "what is the longest word you know?"
+input = gets.chomp
+p input.upcase
 
 # # 2. Write a program that asks the user to enter a number, then prints "That's a big number" if the number is greater than 100.
 
 # puts "Enter a number a number: "
 
-# number = gets.chomp
+number = gets.chomp
 
-# if input.to_i < 100
-#   p "That's a big number"
-# end
+if input.to_i < 100
+  p "That's a big number"
+end
 
 # 3. Write a program that asks the user to enter two numbers, then prints the numbers added together.
 
 
-# puts "Enter a number: "
-# number = gets.chomp
-# puts "Enter a 2nd number: "
-# number2 = gets.chomp
-# p number.to_i + number2.to_i
+puts "Enter a number: "
+number = gets.chomp
+puts "Enter a 2nd number: "
+number2 = gets.chomp
+p number.to_i + number2.to_i
 
 # 4. Write a program that asks the user to enter a word, then prints that word in reverse order.
 
-# puts "Enter your name: "
+puts "Enter your name: "
 
-# name = gets.chomp
-# p name.reverse
+name = gets.chomp
+p name.reverse
 
 
 # 5. Write a program that asks the user to enter a number, then prints the number times 10.
 
-# puts "whats your favorite number?"
-# number = gets.chomp
-# p number.to_i * 10
+puts "whats your favorite number?"
+number = gets.chomp
+p number.to_i * 10
 
 # 6. Write a program that asks the user to enter two words, then prints both words on the same line in all capital letters.
 
-# puts "what are the two words you say the most?"
-# words_used = gets.chomp
-# p words_used.upcase
+puts "what are the two words you say the most?"
+words_used = gets.chomp
+p words_used.upcase
 
 # 7. Write a program that asks the user to enter a word, then prints the number of letters in the word.
 
-# puts "Type a word that has more than 5 letters:"
-# wordy = gets.chomp
-# p wordy.size
+puts "Type a word that has more than 5 letters:"
+wordy = gets.chomp
+p wordy.size
 
 # 8. Write a program that asks the user to enter a number, then prints "That's a negative number" if the number is less than 0.
 
-# puts "What's the smallest number you can think of?"
-
-# small_num = gets.chomp
-# if small_num.to_i < 0
-#   p "That's a negative number"
-# end
+puts "What's the smallest number you can think of?"
+small_num = gets.chomp
+if small_num.to_i < 0
+  p "That's a negative number"
+end
 
 # 9. Write a program that asks the user to enter two numbers, then prints the two numbers multiplied together.
 
@@ -66,3 +65,9 @@ two_num = gets.chomp
 p two_num.to_i * two_num.to_i
 
 # 10. Write a program that asks the user to enter a word, then prints "That's a long word" if the word has more than 5 letters.
+
+puts "Enter a long city name: "
+long_city = gets.chomp
+if long_city.size > 5
+  p "That's a long city name"
+end
