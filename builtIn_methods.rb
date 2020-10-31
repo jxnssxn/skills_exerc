3184f5ffac4dbf573bd702345fbb8c6e363d22a1
jimmy <jonssonjimmy8@gmail.com>
2020-10-31 11:20:10 -0400

    method exercises 1-3

--- builtIn_methods.rb
+++ builtIn_methods.rb
@@ -3,3 +3,16 @@
 puts "what is the longest word you know?"
 input = gets.chomp
 p input.upcase
+
+# 2. Write a program that asks the user to enter a number, then prints "That's a big number" if the number is greater than 100.
+
+puts "Enter a number a number: "
+
+number = gets.chomp
+
+if input.to_i < 100
+  p "That's a big number"
+end
+
+# 3. Write a program that asks the user to enter two numbers, then prints the numbers added together.
+
