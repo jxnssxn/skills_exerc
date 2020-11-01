cd6030e8be59a267fca2af1cb4b097862f64e6cf
jimmy <jonssonjimmy8@gmail.com>
2020-11-01 00:31:51 -0400

    loops exercises 1, 2, 4,5

--- loops_practice.rb
+++ loops_practice.rb
@@ -1,32 +1,30 @@
 # 1. Write a while loop to print the numbers 1 through 10.
 
-# numbers = [1,2,3,4,5,6,7,8,9,10]
+numbers = [1,2,3,4,5,6,7,8,9,10]
 
-# i = 0
-# while i < numbers.length
-#   p numbers[i]
-#   i += 1
-# end
+i = 0
+while i < numbers.length
+  p numbers[i]
+  i += 1
+end
 
 # # 2. Write a while loop that prints the word "hello" 5 times.
 
-# 5.times do
-#   p "hello"
-# end
+5.times do
+  p "hello"
+end
 
 
 # 4. Write a while loop that prints the numbers 0 through 100, increasing by 5 each time.
 
-# i = 0
-# while i < 101
-#   p i 
-#   i = i + 5
-# end
-
-# # 5. Write a while loop that prints the number 9000 ten times.
-# index = 0
-# while index < 10
-#   p 9000 * 10
-#   index += 1
-# end
-
+i = 0
+while i < 101
+  p i 
+  i = i + 5
+end
+# 5. Write a while loop that prints the number 9000 ten times.
+index = 0
+while index < 10
+  p 9000 * 10
+  index += 1
+end
