3663a0fe028878b06ea7771b4f05e1c63ce446c1
jimmy <jonssonjimmy8@gmail.com>
2020-11-01 00:13:41 -0400

    lopps exercise 1 throughu 3

---
+++ loops_practice.rb
@@ -0,0 +1,17 @@
+# 1. Write a while loop to print the numbers 1 through 10.
+
+# numbers = [1,2,3,4,5,6,7,8,9,10]
+
+# i = 0
+# while i < numbers.length
+#   p numbers[i]
+#   i += 1
+# end
+
+# # 2. Write a while loop that prints the word "hello" 5 times.
+
+# 5.times do
+#   p "hello"
+# end
+
+# 3. Write a while loop that asks the user to enter a word and will run forever until the user enters the word "stop".
